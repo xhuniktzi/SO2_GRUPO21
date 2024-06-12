8d6156e471d0cdb1622bfa9b46afbfd8f31ebe0b
Xhunik Miguel <xhuniktzi@gmail.com>
2024-06-12 00:19:44 -0600

    pruebas

--- Practica 2/bank_operations.c
+++ Practica 2/bank_operations.c
@@ -26,15 +26,17 @@ int operacionesHilo2;
 int operacionesHilo3;
 int operacionesHilo4;
 
-struct usuario {
+struct usuario
+{
     int no_cuenta;
     char nombre[100];
     float saldo;
-    struct usuario* siguiente;
+    struct usuario *siguiente;
 };
-struct usuario* cabezaUsuarios = NULL;
+struct usuario *cabezaUsuarios = NULL;
 
-struct operacion {
+struct operacion
+{
     int operacion;
     int cuenta1;
     int cuenta2;
@@ -47,15 +49,17 @@ char erroresOperaciones[10000];
 struct usuario usuarios[MAX_USERS];
 struct operacion operaciones[MAX_OPERATIONS];
 
-typedef struct hilo_info {
+typedef struct hilo_info
+{
     int id;
     json_t *root;
     int start_index;
     int end_index;
 } HiloInfo;
 
-void insertarUsuario(int numeroCuenta, char nombre[100], float saldo) {
-    struct usuario* nuevoNodo = (struct usuario*)malloc(sizeof(struct usuario));
+void insertarUsuario(int numeroCuenta, char nombre[100], float saldo)
+{
+    struct usuario *nuevoNodo = (struct usuario *)malloc(sizeof(struct usuario));
     nuevoNodo->no_cuenta = numeroCuenta;
     strcpy(nuevoNodo->nombre, nombre);
     nuevoNodo->saldo = saldo;
@@ -63,10 +67,13 @@ void insertarUsuario(int numeroCuenta, char nombre[100], float saldo) {
     cabezaUsuarios = nuevoNodo;
 }
 
-bool existeCuenta(int numeroCuenta) {
-    struct usuario* actual = cabezaUsuarios;
-    while (actual != NULL) {
-        if (actual->no_cuenta == numeroCuenta) {
+bool existeCuenta(int numeroCuenta)
+{
+    struct usuario *actual = cabezaUsuarios;
+    while (actual != NULL)
+    {
+        if (actual->no_cuenta == numeroCuenta)
+        {
             return true;
         }
         actual = actual->siguiente;
@@ -74,10 +81,13 @@ bool existeCuenta(int numeroCuenta) {
     return false;
 }
 
-struct usuario* getUsuario(int numeroCuenta) {
-    struct usuario* actual = cabezaUsuarios;
-    while (actual != NULL) {
-        if (actual->no_cuenta == numeroCuenta) {
+struct usuario *getUsuario(int numeroCuenta)
+{
+    struct usuario *actual = cabezaUsuarios;
+    while (actual != NULL)
+    {
+        if (actual->no_cuenta == numeroCuenta)
+        {
             return actual;
         }
         actual = actual->siguiente;
@@ -85,46 +95,61 @@ struct usuario* getUsuario(int numeroCuenta) {
     return NULL;
 }
 
-void deposito(int cuenta, float monto) {
-    struct usuario* usuario = getUsuario(cuenta);
+void deposito(int cuenta, float monto)
+{
+    struct usuario *usuario = getUsuario(cuenta);
     usuario->saldo = usuario->saldo + monto;
 }
 
-void retiro(int cuenta, float monto, int linea) {
-    struct usuario* usuario = getUsuario(cuenta);
+void retiro(int cuenta, float monto, int linea)
+{
+    struct usuario *usuario = getUsuario(cuenta);
     char error[200];
-    if (usuario->saldo - monto >= 0) {
+    if (usuario->saldo - monto >= 0)
+    {
         usuario->saldo = usuario->saldo - monto;
-    } else {
-        if (linea > 0) {
+    }
+    else
+    {
+        if (linea > 0)
+        {
             sprintf(error, "Línea #%d - Saldo insuficiente en cuenta %d\n", linea, cuenta);
             strcat(erroresOperaciones, error);
         }
     }
 }
 
-void transferencia(int cuenta1, int cuenta2, float monto, int linea) {
-    struct usuario* origen = getUsuario(cuenta1);
-    struct usuario* destino = getUsuario(cuenta2);
+void transferencia(int cuenta1, int cuenta2, float monto, int linea)
+{
+    struct usuario *origen = getUsuario(cuenta1);
+    struct usuario *destino = getUsuario(cuenta2);
     char error[200];
-    if (destino != NULL) {
-        if (origen->saldo - monto >= 0) {
+    if (destino != NULL)
+    {
+        if (origen->saldo - monto >= 0)
+        {
             origen->saldo -= monto;
             destino->saldo += monto;
-        } else {
+        }
+        else
+        {
             sprintf(error, "Línea #%d - Saldo insuficiente en cuenta %d\n", linea, cuenta1);
             strcat(erroresOperaciones, error);
         }
-    } else {
+    }
+    else
+    {
         sprintf(error, "Línea #%d - Cuenta destino %d no existe\n", linea, cuenta2);
         strcat(erroresOperaciones, error);
     }
 }
 
-void* cargarUsuarios(void* arg) {
+void *cargarUsuarios(void *arg)
+{
     HiloInfo *info_hilo = (HiloInfo *)arg;
 
-    for (int i = info_hilo->start_index; i < info_hilo->end_index; i++) {
+    for (int i = info_hilo->start_index; i < info_hilo->end_index; i++)
+    {
         json_t *value = json_array_get(info_hilo->root, i);
 
         int cuenta = json_integer_value(json_object_get(value, "no_cuenta"));
@@ -132,18 +157,26 @@ void* cargarUsuarios(void* arg) {
         double saldo = json_real_value(json_object_get(value, "saldo"));
 
         pthread_mutex_lock(&lock);
-        if (existeCuenta(cuenta)) {
+        if (existeCuenta(cuenta))
+        {
             char error[200];
             sprintf(error, "Línea #%d - Cuenta duplicada %d\n", i + 1, cuenta);
             strcat(erroresUsuarios, error);
-        } else {
+        }
+        else
+        {
             insertarUsuario(cuenta, (char *)nombre, (float)saldo);
             usuariosLeidos++;
-            if (info_hilo->id == 1) {
+            if (info_hilo->id == 1)
+            {
                 usuariosHilo1++;
-            } else if (info_hilo->id == 2) {
+            }
+            else if (info_hilo->id == 2)
+            {
                 usuariosHilo2++;
-            } else {
+            }
+            else
+            {
                 usuariosHilo3++;
             }
         }
@@ -154,10 +187,12 @@ void* cargarUsuarios(void* arg) {
     return NULL;
 }
 
-void* cargarOperaciones(void *arg) {
+void *cargarOperaciones(void *arg)
+{
     HiloInfo *info_hilo = (HiloInfo *)arg;
 
-    for (int i = info_hilo->start_index; i < info_hilo->end_index; i++) {
+    for (int i = info_hilo->start_index; i < info_hilo->end_index; i++)
+    {
         json_t *value = json_array_get(info_hilo->root, i);
 
         int operacion = json_integer_value(json_object_get(value, "operacion"));
@@ -166,26 +201,41 @@ void* cargarOperaciones(void *arg) {
         double monto = json_real_value(json_object_get(value, "monto"));
 
         pthread_mutex_lock(&lock);
-        if (!existeCuenta(cuenta1) || (operacion == 3 && !existeCuenta(cuenta2))) {
+        if (!existeCuenta(cuenta1) || (operacion == 3 && !existeCuenta(cuenta2)))
+        {
             char error[200];
             sprintf(error, "Línea #%d - Cuenta no existe %d o %d\n", i + 1, cuenta1, cuenta2);
             strcat(erroresOperaciones, error);
-        } else {
-            if (operacion == 1) {
+        }
+        else
+        {
+            if (operacion == 1)
+            {
                 deposito(cuenta1, (float)monto);
-            } else if (operacion == 2) {
+            }
+            else if (operacion == 2)
+            {
                 retiro(cuenta1, (float)monto, i + 1);
-            } else if (operacion == 3) {
+            }
+            else if (operacion == 3)
+            {
                 transferencia(cuenta1, cuenta2, (float)monto, i + 1);
             }
             operacionesLeidas++;
-            if (info_hilo->id == 1) {
+            if (info_hilo->id == 1)
+            {
                 operacionesHilo1++;
-            } else if (info_hilo->id == 2) {
+            }
+            else if (info_hilo->id == 2)
+            {
                 operacionesHilo2++;
-            } else if (info_hilo->id == 3) {
+            }
+            else if (info_hilo->id == 3)
+            {
                 operacionesHilo3++;
-            } else {
+            }
+            else
+            {
                 operacionesHilo4++;
             }
         }
@@ -196,7 +246,8 @@ void* cargarOperaciones(void *arg) {
     return NULL;
 }
 
-void generarReporteUsuarios() {
+void generarReporteUsuarios()
+{
     char nombre[100];
     char fecha[100];
     time_t tiempo_actual;
@@ -206,7 +257,8 @@ void generarReporteUsuarios() {
     strftime(nombre, sizeof(nombre), "carga_%Y_%m_%d-%H_%M_%S.log", info_tiempo);
     strftime(fecha, sizeof(fecha), "%Y-%m-%d %H:%M:%S", info_tiempo);
     FILE *reporte = fopen(nombre, "w");
-    if (reporte) {
+    if (reporte)
+    {
         fprintf(reporte, "Fecha: %s\n", fecha);
         fprintf(reporte, "Usuarios cargados: %d\n", usuariosLeidos);
         fprintf(reporte, "Hilo 1: %d\n", usuariosHilo1);
@@ -217,7 +269,8 @@ void generarReporteUsuarios() {
     }
 }
 
-void generarReporteOperaciones() {
+void generarReporteOperaciones()
+{
     char nombre[100];
     char fecha[100];
     time_t tiempo_actual;
@@ -227,7 +280,8 @@ void generarReporteOperaciones() {
     strftime(nombre, sizeof(nombre), "operaciones_%Y_%m_%d-%H_%M_%S.log", info_tiempo);
     strftime(fecha, sizeof(fecha), "%Y-%m-%d %H:%M:%S", info_tiempo);
     FILE *reporte = fopen(nombre, "w");
-    if (reporte) {
+    if (reporte)
+    {
         fprintf(reporte, "Fecha: %s\n", fecha);
         fprintf(reporte, "Operaciones cargadas: %d\n", operacionesLeidas);
         fprintf(reporte, "Hilo 1: %d\n", operacionesHilo1);
@@ -239,12 +293,15 @@ void generarReporteOperaciones() {
     }
 }
 
-void estadoDeCuenta() {
-    struct usuario* actual = cabezaUsuarios;
+void estadoDeCuenta()
+{
+    struct usuario *actual = cabezaUsuarios;
     FILE *reporte = fopen("balance.json", "w");
-    if (reporte) {
+    if (reporte)
+    {
         json_t *root = json_array();
-        while (actual != NULL) {
+        while (actual != NULL)
+        {
             json_t *user = json_object();
             json_object_set_new(user, "no_cuenta", json_integer(actual->no_cuenta));
             json_object_set_new(user, "nombre", json_string(actual->nombre));
@@ -258,10 +315,12 @@ void estadoDeCuenta() {
     }
 }
 
-void cargarUsuariosMultithreaded(const char *filename) {
+void cargarUsuariosMultithreaded(const char *filename)
+{
     json_error_t error;
     json_t *root = json_load_file(filename, 0, &error);
-    if (!root) {
+    if (!root)
+    {
         fprintf(stderr, "Error al cargar archivo JSON: %s\n", error.text);
         return;
     }
@@ -271,7 +330,8 @@ void cargarUsuariosMultithreaded(const char *filename) {
 
     pthread_t threads[THREADS_USERS];
     HiloInfo info[THREADS_USERS];
-    for (int i = 0; i < THREADS_USERS; i++) {
+    for (int i = 0; i < THREADS_USERS; i++)
+    {
         info[i].id = i + 1;
         info[i].root = root;
         info[i].start_index = i * chunk_size;
@@ -279,7 +339,8 @@ void cargarUsuariosMultithreaded(const char *filename) {
         pthread_create(&threads[i], NULL, cargarUsuarios, &info[i]);
     }
 
-    for (int i = 0; i < THREADS_USERS; i++) {
+    for (int i = 0; i < THREADS_USERS; i++)
+    {
         pthread_join(threads[i], NULL);
     }
 
@@ -287,10 +348,12 @@ void cargarUsuariosMultithreaded(const char *filename) {
     generarReporteUsuarios();
 }
 
-void cargarOperacionesMultithreaded(const char *filename) {
+void cargarOperacionesMultithreaded(const char *filename)
+{
     json_error_t error;
     json_t *root = json_load_file(filename, 0, &error);
-    if (!root) {
+    if (!root)
+    {
         fprintf(stderr, "Error al cargar archivo JSON: %s\n", error.text);
         return;
     }
@@ -300,7 +363,8 @@ void cargarOperacionesMultithreaded(const char *filename) {
 
     pthread_t threads[THREADS_OPERATIONS];
     HiloInfo info[THREADS_OPERATIONS];
-    for (int i = 0; i < THREADS_OPERATIONS; i++) {
+    for (int i = 0; i < THREADS_OPERATIONS; i++)
+    {
         info[i].id = i + 1;
         info[i].root = root;
         info[i].start_index = i * chunk_size;
@@ -308,7 +372,8 @@ void cargarOperacionesMultithreaded(const char *filename) {
         pthread_create(&threads[i], NULL, cargarOperaciones, &info[i]);
     }
 
-    for (int i = 0; i < THREADS_OPERATIONS; i++) {
+    for (int i = 0; i < THREADS_OPERATIONS; i++)
+    {
         pthread_join(threads[i], NULL);
     }
 
@@ -316,7 +381,8 @@ void cargarOperacionesMultithreaded(const char *filename) {
     generarReporteOperaciones();
 }
 
-void mostrarMenu() {
+void mostrarMenu()
+{
     printf("1. Cargar usuarios\n");
     printf("2. Cargar operaciones\n");
     printf("3. Operaciones individuales\n");
@@ -324,84 +390,101 @@ void mostrarMenu() {
     printf("5. Salir\n");
 }
 
-void manejarEntradaUsuario() {
+void manejarEntradaUsuario()
+{
     int opcion, cuenta1, cuenta2;
     double monto;
 
-    while (1) {
+    while (1)
+    {
         mostrarMenu();
         scanf("%d", &opcion);
 
-        switch (opcion) {
+        switch (opcion)
+        {
+        case 1:
+            cargarUsuariosMultithreaded("users.json");
+            break;
+        case 2:
+            cargarOperacionesMultithreaded("operations.json");
+            break;
+        case 3:
+            printf("1. Deposito\n2. Retiro\n3. Transferencia\n4. Consultar cuenta\n");
+            scanf("%d", &opcion);
+            switch (opcion)
+            {
             case 1:
-                cargarUsuariosMultithreaded("users.json");
+                printf("Cuenta: ");
+                scanf("%d", &cuenta1);
+                printf("Monto: ");
+                scanf("%lf", &monto);
+                if (existeCuenta(cuenta1))
+                {
+                    deposito(cuenta1, monto);
+                }
+                else
+                {
+                    printf("Cuenta no encontrada\n");
+                }
                 break;
             case 2:
-                cargarOperacionesMultithreaded("operations.json");
+                printf("Cuenta: ");
+                scanf("%d", &cuenta1);
+                printf("Monto: ");
+                scanf("%lf", &monto);
+                if (existeCuenta(cuenta1))
+                {
+                    retiro(cuenta1, monto, 0);
+                }
+                else
+                {
+                    printf("Cuenta no encontrada\n");
+                }
                 break;
             case 3:
-                printf("1. Deposito\n2. Retiro\n3. Transferencia\n4. Consultar cuenta\n");
-                scanf("%d", &opcion);
-                switch (opcion) {
-                    case 1:
-                        printf("Cuenta: ");
-                        scanf("%d", &cuenta1);
-                        printf("Monto: ");
-                        scanf("%lf", &monto);
-                        if (existeCuenta(cuenta1)) {
-                            deposito(cuenta1, monto);
-                        } else {
-                            printf("Cuenta no encontrada\n");
-                        }
-                        break;
-                    case 2:
-                        printf("Cuenta: ");
-                        scanf("%d", &cuenta1);
-                        printf("Monto: ");
-                        scanf("%lf", &monto);
-                        if (existeCuenta(cuenta1)) {
-                            retiro(cuenta1, monto, 0);
-                        } else {
-                            printf("Cuenta no encontrada\n");
-                        }
-                        break;
-                    case 3:
-                        printf("Cuenta origen: ");
-                        scanf("%d", &cuenta1);
-                        printf("Cuenta destino: ");
-                        scanf("%d", &cuenta2);
-                        printf("Monto: ");
-                        scanf("%lf", &monto);
-                        if (existeCuenta(cuenta1) && existeCuenta(cuenta2)) {
-                            transferencia(cuenta1, cuenta2, monto, 0);
-                        } else {
-                            printf("Cuenta no encontrada\n");
-                        }
-                        break;
-                    case 4:
-                        printf("Cuenta: ");
-                        scanf("%d", &cuenta1);
-                        struct usuario* usuario = getUsuario(cuenta1);
-                        if (usuario) {
-                            printf("Cuenta: %d, Nombre: %s, Saldo: %.2f\n", usuario->no_cuenta, usuario->nombre, usuario->saldo);
-                        } else {
-                            printf("Cuenta no encontrada\n");
-                        }
-                        break;
+                printf("Cuenta origen: ");
+                scanf("%d", &cuenta1);
+                printf("Cuenta destino: ");
+                scanf("%d", &cuenta2);
+                printf("Monto: ");
+                scanf("%lf", &monto);
+                if (existeCuenta(cuenta1) && existeCuenta(cuenta2))
+                {
+                    transferencia(cuenta1, cuenta2, monto, 0);
+                }
+                else
+                {
+                    printf("Cuenta no encontrada\n");
                 }
                 break;
             case 4:
-                estadoDeCuenta();
+                printf("Cuenta: ");
+                scanf("%d", &cuenta1);
+                struct usuario *usuario = getUsuario(cuenta1);
+                if (usuario)
+                {
+                    printf("Cuenta: %d, Nombre: %s, Saldo: %.2f\n", usuario->no_cuenta, usuario->nombre, usuario->saldo);
+                }
+                else
+                {
+                    printf("Cuenta no encontrada\n");
+                }
                 break;
-            case 5:
-                return;
-            default:
-                printf("Opción no válida\n");
+            }
+            break;
+        case 4:
+            estadoDeCuenta();
+            break;
+        case 5:
+            return;
+        default:
+            printf("Opción no válida\n");
         }
     }
 }
 
-int main() {
+int main()
+{
     pthread_mutex_init(&lock, NULL);
 
     manejarEntradaUsuario();
